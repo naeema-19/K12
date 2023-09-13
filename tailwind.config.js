b0e0613f99ac062c5f07c880b7bd16aef081a5dc
NAEEMA ZIYAD <75922224+naeema-19@users.noreply.github.com>
2023-09-13 14:59:28 +0530

    INITIAL

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      dropShadow: {
+        green: '0 0 10rem #ffffff',
+      },
+    },
+    colors: {
+      primary: {
+        400: '#1674d2',
+        500: '#00c2ff',
+        600: '#1a2ab7',
+        700: '#0f3875'
+      },
+      grayscale: {
+        50: '#ffffff',
+        100: '#efefef',
+        200: '#FDFDFF',
+        950: '#16161b',
+      },
+    },
+  },
+  plugins: [tailwindcss, autoprefixer],
+};
