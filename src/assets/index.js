8a15a35d5b3d9c7c2774117bcd17e50be26f5c99
NAEEMA ZIYAD <75922224+naeema-19@users.noreply.github.com>
2023-09-16 23:28:50 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,94 @@
+/*
+   Copyright (C), 2023-2024, Sara Echeverria (bl33h)
+   Author: Sara Echeverria
+   FileName: index.js
+   Version: I
+   Creation: 02/06/2023
+   Last modification: 03/06/2023
+*/
+
+// Logo
+import bl33hIcon from './logo/bl33hIcon.png';
+import kkk from './logo/kkk.png';
+
+// Technologies
+import ho from './tech/ho.png';
+import htmlIcon from './tech/html.png';
+import cssIcon from './tech/css.png';
+import jsIcon from './tech/javascript.png';
+import reactIcon from './tech/reactjs.png';
+import awsIcon from './tech/aws.png';
+import javaIcon from './tech/java.png';
+import gitIcon from './tech/git.png';
+import githubIcon from './tech/github.png';
+import figmaIcon from './tech/figma.png';
+import psqlIcon from './tech/postgresql.png';
+import eslintIcon from './tech/eslint.png';
+import viteIcon from './tech/vite.svg';
+import pyIcon from './tech/py.png';
+import nodeIcon from './tech/node.webp';
+import neoIcon from './tech/neo.png';
+import raspIcon from './tech/raspi.png';
+import close from './close.svg';
+import menu from './menu.svg';
+import randd from './partners/randd.png';
+import sight from './partners/sight.png';
+import signalgen from './modules/1.jpg';
+import ultra  from './modules/3.jpg';
+import opamp  from './modules/4.jpg';
+import multi  from './modules/5.jpg';
+import irs  from './modules/7.jpg';
+import tools  from './modules/8.jpg';
+import fg  from './modules/9.jpg';
+import  ard from './modules/2.jpg';
+import  cap from './modules/6.jpg';
+import tkm from './partners/tkm-logo.png';
+
+// Projects
+import pawsitivePrototype from './projects/pawsitive.jpeg';
+import calculator from './projects/calculator.jpg';
+import memoryGame from './projects/memory.jpg';
+
+// Avatar
+import avatar from './avatar/avatar.png';
+
+export {
+  menu,
+  close,
+  htmlIcon,
+  cssIcon,
+  jsIcon,
+  reactIcon,
+  awsIcon,
+  javaIcon,
+  gitIcon,
+  githubIcon,
+  eslintIcon,
+  figmaIcon,
+  psqlIcon,
+  nodeIcon,
+  pyIcon,
+  viteIcon,
+  neoIcon,
+  raspIcon,
+  pawsitivePrototype,
+  calculator,
+  memoryGame,
+  avatar,
+  bl33hIcon,
+  kkk,
+  ho,
+  randd,
+  sight,
+  signalgen,
+  ultra,
+  opamp,
+  multi,
+  irs,
+  tools,
+  fg,
+  ard,
+  cap,
+  tkm
+  
+};
